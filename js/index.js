a8e2ee2ad859d929ccc2eb54a30565bd35ac10f9
VisualYuki <comedy951@yandex.ru>
2020-01-18 15:14:12 +0300

    3:14 PM

--- js/index.js
+++ js/index.js
@@ -4,7 +4,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
  =====================*/
 
   // Класс для получения случайной карты из 104 без повторения.
-  class cardSet {
+  class CardSet {
     // Массивы содержащие по 14 карт каждой масти
     clubsImgArray = [];
     spadesImgArray = [];
@@ -15,10 +15,15 @@ document.addEventListener("DOMContentLoaded", function(event) {
     oneSuitArray = [];
     twoSuitArray = [];
     fourSuitArray = [];
+    currentArray = [];
 
     // CONSTS
     DOUBLE_CARD_NUMBER = 104; // количество карт для двух колод
     ONE_SUIT_CARD_NUM = 13; // количество карт с одной масти
+
+    //Параметры для текущей колоды
+    suitTotal = 1;
+    currentArrayIndex = 0;
 
     constructor() {
       //Добавляем путь изображения в каждый массив 4-ех мастей
@@ -42,7 +47,7 @@ document.addEventListener("DOMContentLoaded", function(event) {
                 this.diamondsImgArray.length
               ] = `../images/cards/${suit}/${this.cardImgIndex}.png`;
               break;
-            case "diamonds":
+            case "hearts":
               this.heartsImgArray[
                 this.heartsImgArray.length
               ] = `../images/cards/${suit}/${this.cardImgIndex}.png`;
@@ -52,77 +57,79 @@ document.addEventListener("DOMContentLoaded", function(event) {
       }
 
       this.createOneSuitArray();
-
       this.createTwoSuitArray();
-    }
-
-    addArrayElement(value, index, array) {
-      array[index] = value;
+      this.createFourSuitArray();
+      this.setSuitTotal_In_Deck(1);
     }
 
     //Заполняем массив из 104 карт колодой из одной масти
     createOneSuitArray() {
-      for (let i = 0; i < this.ONE_SUIT_CARD_NUM; i++) {
-        for (let j = 0; j < 8; j++) {
-          this.addArrayElement(
-            this.spadesImgArray[i],
-            this.ONE_SUIT_CARD_NUM * j + i,
-            this.oneSuitArray
-          );
-        }
-      }
+      this.oneSuitArray = this.spadesImgArray
+        .concat(this.spadesImgArray)
+        .concat(this.spadesImgArray)
+        .concat(this.spadesImgArray)
+        .concat(this.spadesImgArray)
+        .concat(this.spadesImgArray)
+        .concat(this.spadesImgArray)
+        .concat(this.spadesImgArray);
     }
 
     //Заполняем массив из 104 карт колодой из двух мастей
     createTwoSuitArray() {
-      for (let i = 0; i < this.ONE_SUIT_CARD_NUM; i++) {
-        for (let j = 0; j < 4; j++) {
-          this.addArrayElement(
-            this.spadesImgArray[i],
-            this.ONE_SUIT_CARD_NUM * j + i,
-            this.twoSuitArray
-          );
-        }
-        for (let j = 0; j < 4; j++) {
-          this.addArrayElement(
-            this.diamondsImgArray[i],
-            this.ONE_SUIT_CARD_NUM * j + i + 52,
-            this.twoSuitArray
-          );
-        }
-      }
+      this.twoSuitArray = this.spadesImgArray
+        .concat(this.spadesImgArray)
+        .concat(this.spadesImgArray)
+        .concat(this.spadesImgArray)
+        .concat(this.diamondsImgArray)
+        .concat(this.diamondsImgArray)
+        .concat(this.diamondsImgArray)
+        .concat(this.diamondsImgArray);
     }
 
-    //! доделать
     //Заполняем массив из 104 карт колодой из четырех мастей
     createFourSuitArray() {
-      for (let i = 0; i < this.ONE_SUIT_CARD_NUM; i++) {
-        for (let j = 0; j < 4; j++) {
-          this.addArrayElement(
-            this.spadesImgArray[i],
-            this.ONE_SUIT_CARD_NUM * j + i,
-            this.twoSuitArray
-          );
-        }
-        for (let j = 0; j < 4; j++) {
-          this.addArrayElement(
-            this.diamondsImgArray[i],
-            this.ONE_SUIT_CARD_NUM * j + i + 52,
-            this.twoSuitArray
-          );
-        }
-      }
+      this.fourSuitArray = this.clubsImgArray
+        .concat(this.clubsImgArray)
+        .concat(this.diamondsImgArray)
+        .concat(this.diamondsImgArray)
+        .concat(this.heartsImgArray)
+        .concat(this.heartsImgArray)
+        .concat(this.spadesImgArray)
+        .concat(this.spadesImgArray);
     }
 
-    //Перемешиваем карты в колоде
+    //Устанавливаем количество мастей в колоде
+    setSuitTotal_In_Deck(value = 1) {
+      if (value == 1 || value == 2 || value == 4) {
+        this.suitTotal = value;
+        switch (this.suitTotal) {
+          case 1:
+            this.currentArray = this.oneSuitArray;
+            break;
+          case 2:
+            this.currentArray = this.twoSuitArray;
+            break;
+          case 4:
+            this.currentArray = this.fourSuitArray;
+            break;
+        }
+      } else console.log("Неверное количество мастей в setSuitTotal()");
+    }
+
+    // Получаем следующую случайную карту
+    getNextRandCard() {
+      return this.currentArray[this.currentArrayIndex];
+    }
+
+    //Перемешиваем карты в текущей колоде
     shuffleCards(repeatNumber = 1) {
       if (repeatNumber < 1) repeatNumber = 1;
-
+      this.currentArrayIndex = 0;
       for (let j = 1; j < repeatNumber; j++) {
         for (let i = 0; i < this.DOUBLE_CARD_NUMBER; i++) {
           let randCardIndex =
             Math.trunc(Math.random() * 1000) % this.DOUBLE_CARD_NUMBER;
-          this.swapTwoCard(i, randCardIndex, this.cardDoubleArray);
+          this.swapTwoCard(i, randCardIndex, this.currentArray);
         }
       }
     }
@@ -133,12 +140,6 @@ document.addEventListener("DOMContentLoaded", function(event) {
       array[indexA] = array[indexB];
       array[indexB] = timeCopy;
     }
-
-    // Получаем следующую случайную карту
-    getNextRandCard() {
-      if (this.cardDoubleArray.length != 0) return this.cardDoubleArray.pop();
-      else console.log("cardDoubleArray is empty to pop");
-    }
   }
 
   /*===================
@@ -147,36 +148,43 @@ document.addEventListener("DOMContentLoaded", function(event) {
 
   // Класс колода для раздачи карт по колонкам.
   class Deck {
+    //Константы
     SET_NUM = 6; // количество карт в колоде для раздачи
     SET_CARD_NUM = 10; //количество карт при раздаче на все колонки
 
-    cardDeckElement = $("#card-deck");
+    cardDeckElement = document.getElementById("card-deck");
 
+    //Параметры текущей колоды
     currentSetIndex = -1;
     cardSets = [];
+    set;
 
-    constructor() {
-      //Заполняем наборы карт для распределения по колонкам.
+    constructor(set) {
+      this.set = set;
+      //Заполняем наборы карт для распределения по колонкам. Всего 6 наборов
       for (let i = 0; i < this.SET_NUM; i++) {
         this.cardSets[i] = [];
         for (let j = 0; j < this.SET_CARD_NUM; j++) {
           this.cardSets[i][j] = set.getNextRandCard();
         }
       }
-      //this.pushSet();
-      this.fillFullDeck();
-    }
 
-    fillFullDeck() {
+      this.fillDeckFull_OnBoard();
+    }
+    //Заполняет колоды полностью ( 6 карт)
+    fillDeckFull_OnBoard() {
       for (let i = 0; i < this.SET_NUM; i++) {
-        this.pushSet();
+        this.addCard_OnBoard();
       }
     }
 
-    popSet() {}
+    //Удаляет последную карту в колоде
+    removeCard_OnBoard() {
+      this.cardDeckElement.lastChild.remove();
+    }
 
-    // Добавляем элемент карта к колоде
-    pushSet() {
+    // Добавляем карту в колоду
+    addCard_OnBoard() {
       this.currentSetIndex++;
 
       let element = document.createElement("li");
@@ -185,19 +193,51 @@ document.addEventListener("DOMContentLoaded", function(event) {
       element.style.zIndex = this.currentSetIndex;
       element.addEventListener("click", this.handOutCards);
 
-      $(this.cardDeckElement).append(element);
+      this.cardDeckElement.appendChild(element);
     }
 
     handOutCards() {}
   }
 
-  set = new cardSet();
-  //deck = new Deck();
-  //console.log(set.getNextRandCard());
+  class Column {
+    constructor() {}
+  }
+
+  //Класс для текущего времени
+  class Timer {
+    timerElement = document.getElementById("timerElement");
+    seconds = 0;
+    minuts = 0;
+
+    //Добавляем ноль, если число состояит из одной цирфы
+    formatDateNumber(number) {
+      if (number < 10) return "0" + number;
+      else return number;
+    }
+
+    //! доделать.
+    //Устанавливаем интервал для обновления времени
+    startTimer() {
+      setInterval(function() {
+        this.seconds++;
+        if (this.seconds < 60) {
+          this.minuts++;
+          this.seconds = 0;
+        }
+        this.timerElement.textContent =
+          this.formatDateNumber(this.minuts) +
+          ":" +
+          formatDateNumber(this.seconds);
+        this.minuts;
+      }, 1000);
+    }
+
+    //Обнуляем время таймера
+    setDefaultValue() {}
+  }
+
+  let cardSet = new CardSet();
+  let deck = new Deck(cardSet);
+  let timer = new Timer();
+  timer.startTimer();
 });
-
-// let textImg = new Image();
-
-// textImg.src = "../images/" + "cards/clubs/" + "03.png";
-
-// document.getElementById("testId").append(textImg);
